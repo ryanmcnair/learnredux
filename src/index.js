988e2529b8966398f43a7542f3323a46c5f0e92f
Ryan McNair <ryan.mcnair@gmail.com>
2021-06-29 15:01:55 -0500

    added a new button to show the secret info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable default-case */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION -> A FUNCTION THAT RETURNS AN OBJECT
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+//REDUCER ->
+// const counter = (state = 0, action) => {
+//     switch(action.type){
+//       case 'INCREMENT':
+//         return state + 1;
+//       case 'DECREMENT':
+//         return state - 1;
+//     }
+// };
+
+// let store = createStore(counter);
+//DISPLAY IT IN THE CONSOLE
+// store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+// store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
